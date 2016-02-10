7026f88d9149fc51e2730f6c602e9480e435a825
Simone Fontana <fontana.simone@outlook.com>
2016-02-10 18:11:43 +0100

    Add ground_truth comparison Add voxel filtering

--- src/point_cloud_registration_ros.cc
+++ src/point_cloud_registration_ros.cc
@@ -3,11 +3,14 @@
 
 #include <Eigen/Sparse>
 #include <pcl/common/transforms.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <ros/ros.h>
+
 
 #include "point_cloud_registration/point_cloud_registration.h"
 
@@ -24,10 +27,12 @@ int main(int argc, char** argv)
     ros::Publisher aligned_source_pub = n.advertise<pcl::PointCloud<PointType>>("aligned_source", 1);
     ros::Publisher source_pub = n.advertise<pcl::PointCloud<PointType>>("source_cloud", 1);
     ros::Publisher target_pub = n.advertise<pcl::PointCloud<PointType>>("target_cloud", 1);
+    ros::Publisher ground_truth_pub = n.advertise<pcl::PointCloud<PointType>>("ground_truth", 1);
 
-    int dim_neighborhood;
-    ros::param::param<int>("~dim_neighborhood", dim_neighborhood, 10);
-    ROS_INFO("Dimension of neighborhood: %d", dim_neighborhood);
+
+    int max_neighbours;
+    ros::param::param<int>("~max_neighbours", max_neighbours, 10);
+    ROS_INFO("Max dimension of neighborhood: %d", max_neighbours);
 
     bool use_gaussian;
     double dof;
@@ -79,16 +84,56 @@ int main(int argc, char** argv)
     {
         ROS_INFO("Using file %s as target point cloud", target_file_name.c_str());
     }
+    target_cloud->header.frame_id="map";
+    target_pub.publish(target_cloud);
 
+
+    bool ground_truth = false;
+    ROS_INFO("Loading target point cloud");
+    pcl::PointCloud<PointType>::Ptr source_ground_truth =
+        boost::make_shared<pcl::PointCloud<PointType>>();
+    std::string ground_truth_file;
+    if (ros::param::get("~ground_truth", ground_truth_file) == false ||
+            pcl::io::loadPCDFile<PointType>(ground_truth_file, *source_ground_truth) == -1)
+    {
+        ROS_INFO("Could not load ground truth...");
+    }
+    else
+    {
+        ROS_INFO("Using file %s as ground truth point cloud", ground_truth_file.c_str());
+        ground_truth = true;
+    }
+    source_ground_truth->header.frame_id="map";
+    ground_truth_pub.publish(source_ground_truth);
+
+
+    double source_filter_size, target_filter_size;
+    ros::param::param<double>("~source_filter_size", source_filter_size, 0);
+    ROS_INFO("The leaf size of the voxel filter of the source cloud is: %f",source_filter_size);
+    ros::param::param<double>("~target_filter_size", target_filter_size, 0);
+    ROS_INFO("The leaf size of the voxel filter of the dense cloud : %f", target_filter_size);
+    pcl::VoxelGrid<PointType> filter;
+    pcl::PointCloud<PointType>::Ptr filtered_source_cloud =
+        boost::make_shared<pcl::PointCloud<PointType>>();
+    if (source_filter_size > 0) {
+        filter.setInputCloud(source_cloud);
+        filter.setLeafSize(source_filter_size, source_filter_size, source_filter_size);
+        filter.filter(*filtered_source_cloud);
+      }
+      if (target_filter_size > 0) {
+        filter.setInputCloud(target_cloud);
+        filter.setLeafSize(target_filter_size, target_filter_size, target_filter_size);
+        filter.filter(*target_cloud);
+      }
     pcl::KdTreeFLANN<PointType> kdtree;
     kdtree.setInputCloud(target_cloud);
     std::vector<float> distances;
-    Eigen::SparseMatrix<int, Eigen::RowMajor> data_association(source_cloud->size(), target_cloud->size());
+    Eigen::SparseMatrix<int, Eigen::RowMajor> data_association(filtered_source_cloud->size(), target_cloud->size());
     std::vector<Eigen::Triplet<int>> tripletList;
-    for (std::size_t i = 0; i < source_cloud->size(); i++)
+    for (std::size_t i = 0; i < filtered_source_cloud->size(); i++)
     {
         std::vector<int> neighbours;
-        kdtree.radiusSearch(*source_cloud, i, radius, neighbours, distances, dim_neighborhood);
+        kdtree.radiusSearch(*filtered_source_cloud, i, radius, neighbours, distances, max_neighbours);
         for (int j : neighbours)
         {
             tripletList.push_back(Eigen::Triplet<int>(i, j, 1));
@@ -99,15 +144,15 @@ int main(int argc, char** argv)
 
     PointCloudRegistrationParams params;
     params.dof = std::numeric_limits<double>::infinity();
-    params.max_neighbours = dim_neighborhood;
+    params.max_neighbours = max_neighbours;
     params.dimension = 3;
-    PointCloudRegistration registration(*source_cloud, *target_cloud, data_association, params);
+    PointCloudRegistration registration(*filtered_source_cloud, *target_cloud, data_association, params);
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY;
     options.use_nonmonotonic_steps = true;
     options.minimizer_progress_to_stdout = true;
     options.max_num_iterations = std::numeric_limits<int>::max();
-    options.function_tolerance = 10e-5;
+    options.function_tolerance = 10e-10;
     options.num_threads = 8;
     ceres::Solver::Summary summary;
     registration.solve(options, &summary);
@@ -117,10 +162,30 @@ int main(int argc, char** argv)
     pcl::PointCloud<PointType>::Ptr aligned_source = boost::make_shared<pcl::PointCloud<PointType>>();
     pcl::transformPointCloud (*source_cloud, *aligned_source, estimated_transform);
     source_cloud->header.frame_id="map";
-    source_pub.publish(source_cloud);
     target_cloud->header.frame_id="map";
-    target_pub.publish(target_cloud);
     aligned_source->header.frame_id="map";
-    aligned_source_pub.publish(aligned_source);
+
+    if(ground_truth){
+        double mean_error = 0;
+        for (std::size_t i = 0; i < source_ground_truth->size(); ++i)
+        {
+            double error = std::sqrt(std::pow(source_ground_truth->at(i).x - aligned_source->at(i).x, 2) +
+                                     std::pow(source_ground_truth->at(i).y - aligned_source->at(i).y, 2) +
+                                     std::pow(source_ground_truth->at(i).z - aligned_source->at(i).z, 2));
+            mean_error += error;
+        }
+        mean_error /= target_cloud->size();
+        ROS_INFO("Mean error: %f", mean_error);
+    }
+    ros::Rate rate(1);
+    while(ros::ok()){
+        source_pub.publish(source_cloud);
+        target_pub.publish(target_cloud);
+        aligned_source_pub.publish(aligned_source);
+        if(ground_truth){
+            ground_truth_pub.publish(source_ground_truth);
+        }
+        rate.sleep();
+    }
     return 0;
 }
