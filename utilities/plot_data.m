0f6be8f98400a1deb62dc4533a6fb816ca25680f
Simone Fontana <fontana.simone@outlook.com>
2016-11-04 18:26:11 +0100

    Fix error in variable name

--- utilities/plot_data.m
+++ utilities/plot_data.m
@@ -1,43 +1,43 @@
 data = csvread('debug_file.txt',1,0);
 
 subplot(3,2,1)
-plot(m(:,1),m(:,2))
+plot(data(:,1),data(:,2))
 ylabel('N success steps')
 
 subplot(3,2,2)
 yyaxis left
-plot(m(:,1),m(:,3))
+plot(data(:,1),data(:,3))
 ylabel('Initial cost')
 yyaxis right
-plot(m(:,1),m(:,4))
+plot(data(:,1),data(:,4))
 ylabel('Final cost')
 
 subplot(3,2,3)
-drop = m(:,3) - m(:,4);
-plot(m(:,1),drop)
+drop = data(:,3) - data(:,4);
+plot(data(:,1),drop)
 ylabel('Cost drop')
 
 subplot(3,2,4)
-plot(m(:,1),m(:,5))
+plot(data(:,1),data(:,5))
 hold on;
-plot(m(:,1),m(:,6))
+plot(data(:,1),data(:,6))
 hold on;
-plot(m(:,1),m(:,7))
+plot(data(:,1),data(:,7))
 ylabel('Translation')
 
 subplot(3,2,5)
-plot(m(:,1),m(:,8))
+plot(data(:,1),data(:,8))
 hold on;
-plot(m(:,1),m(:,9))
+plot(data(:,1),data(:,9))
 hold on;
-plot(m(:,1),m(:,10))
+plot(data(:,1),data(:,10))
 hold on;
 ylabel('Rotation')
 
 subplot(3,2,6)
 yyaxis left;
-plot(m(:,1),m(:,11))
+plot(data(:,1),data(:,11))
 ylabel('MSE previous')
 yyaxis right;
-plot(m(:,1),m(:,12))
+plot(data(:,1),data(:,12))
 ylabel('MSE gtruth')
